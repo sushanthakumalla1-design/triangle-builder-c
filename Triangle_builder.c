bbd87fb8895dbd9fd8646a6df1efd6c337262b1f
Sushanth <sushanthakumalla1@gmail.com>
2025-11-06 12:32:26 +0530

    Code commit

---
+++ Triangle_builder.c
@@ -0,0 +1,39 @@
+# include<stdio.h>
+
+void print_brick_row(int space, int brick);
+
+int main ()
+{
+    printf("Enter the height of the triangle: ");
+    int n;
+    scanf("%i",&n);
+
+    if (n>1)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            print_brick_row(n-i, i);
+        }
+        return 0;
+    }
+    else 
+    {
+        printf("i dont understand, sorry i cant do that!!");
+        return 1;
+    }  
+}
+
+void print_brick_row(int space, int brick)
+{
+    for(int i=0;i<space;i++)
+    {
+        printf(" ");
+    }
+    for(int j=0;j<brick;j++)
+    {
+        printf("#");
+    }
+    printf("\n");
+
+
+}
